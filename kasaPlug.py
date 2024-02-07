3ca30a81bbf24ee84e0ed7444e646fc7d4ab0022
Hongjian Zhu <hongjian.zhu@accuenergy.com>
2024-02-07 14:46:31 -0500

    change slow power cycle time to 90s

--- kasaPlug.py
+++ kasaPlug.py
@@ -54,7 +54,7 @@ class KasaSmartPlug():
       await self.dev.turn_off()
       await asyncio.sleep(2)
       await self.dev.turn_on()
-      time.sleep(60)
+      time.sleep(90)
     
   async def powerCycleSuperSlow(self):
     await self.dev.update()
